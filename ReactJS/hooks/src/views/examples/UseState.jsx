7f0f808455c2b321c0fd5bbbb0ba350e857a6abd
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-09-05 02:20:55 -0300

    Conceito de useState

--- ReactJS/hooks/src/views/examples/UseState.jsx
+++ ReactJS/hooks/src/views/examples/UseState.jsx
@@ -1,13 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import SectionTitle from '../../components/layout/SectionTitle'
+
 const UseState = (props) => {
+    const [count, setCount] = useState(0)
+    const [name, setName] =useState('Digite algo...')
+
     return (
         <div className="UseState">
             <PageTitle
                 title="Hook UseState"
                 subtitle="Estado em componentes funcionais!"
             />
+            <SectionTitle title='Exercicio #01' />
+            <div className='center'>
+                <span className="text">
+                    {count}
+                </span>
+                <div>
+                    <button 
+                        className="btn"
+                        onClick={() => setCount(count + 1)}
+                        >
+                        + 1
+                    </button>
+                    <button 
+                        className="btn"
+                        onClick={() => setCount(count - 1)}
+                        >
+                        - 1
+                    </button>
+                    <button 
+                        className="btn"
+                        onClick={() => setCount(e => e - 10)}
+                        >
+                        - 10
+                    </button>
+                </div>
+            </div>
+            <SectionTitle title='Exercicio #01' />
+            
+            <span className="text">{name}</span>
+            <input 
+                type="text" 
+                className="input" 
+                value={name}
+                onChange={e => setName(e.target.value)}
+                />
         </div>
     )
 }
