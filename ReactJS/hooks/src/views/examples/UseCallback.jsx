79bcc3e07d9660aaf8177f9d717241980936aab7
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-09-05 20:28:32 -0300

    Conceito de UseCallback

--- ReactJS/hooks/src/views/examples/UseCallback.jsx
+++ ReactJS/hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, {useCallback, useState} from 'react'
 
-const UseCallback = (props) => {
+import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallbackButtons'
+
+const UseCallback = (props) =>{
+    const [count, setCount] = useState(0)
+
+    const inc = useCallback(function (delta){
+        setCount(curr => curr + delta)
+    }, [setCount])
+
+    /*function inc(delta){
+        setCount(count + delta)
+    }*/
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className='center'>
+                <span className='text'>{count}</span>
+                <UseCallbackButtons inc={inc}/>
+            </div>
         </div>
     )
 }
