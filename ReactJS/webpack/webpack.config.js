f113ea99e50b153b619f0a70ac883fe3014de4d6
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-12-07 06:33:55 -0300

    Exercícios Webpack parte 2

--- ReactJS/webpack/webpack.config.js
+++ ReactJS/webpack/webpack.config.js
@@ -1,6 +1,8 @@
 const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
     mode: modoDev ? 'development' : 'production',
@@ -9,7 +11,20 @@ module.exports = {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
-    plugins:[
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
         new MiniCssExtractPlugin({
             filename: "estilo.css"
         })
@@ -19,9 +34,13 @@ module.exports = {
             test: /\.s?[ac]ss$/,
             use: [
                 MiniCssExtractPlugin.loader,
-                //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
                 'css-loader', // interpreta @import, url()...
+                'sass-loader',
             ]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
         }]
     }
 }
