865a6dd2edeb63a783f4b9bc0cb06b03a51a3dbc
Odair Cesar Ferreira <odairferreira97@gmail.com>
2022-10-11 12:07:08 -0300

    Projeto tasks #04 - Iniciando a criação do back-end

---
+++ ReactNative/tasks/back-end/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res, next) => {
+  console.log('Func 0')
+  next()
+})
+
+app.get('/', (req, res, next) => {
+  console.log('Func 1')
+  res.status(200).send("Meu Backend")
+})
+
+app.listen(3000, () => {
+  console.log('Back-End iniciado com sucesso')
+})
